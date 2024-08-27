f907fd46f9ebf71e6b88375822de4057ebd4d16f
DaniKesede <danieldsousa76@gmail.com>
2024-08-27 20:39:08 -0300

    Tipos de seleção

---
+++ find_where.mongodb.js
@@ -0,0 +1,10 @@
+/* NOME DO BANCO DE DADOS */
+const database ='BD3-DANIEL-AULA';
+
+/* NOME DO BANCO DA COEÇÃO DE DADOS */
+const collection ='LIVRARIA';
+
+/* CRIAR OU ACESSAR O BANCO DE DADOS */
+use(database);
+
+db[collection].find({"categoria":"Ficção Científica"});
